fb996659b0c23f17d76fc12f4b6480da7e56a6c7
LMN <m.loulou9311@gmail.com>
2023-03-14 10:58:23 +0100

    init sidebar

---
+++ templates/sidebar/index.php
@@ -0,0 +1,9 @@
+<link href="/orange/templates/sidebar/style.css" rel="stylesheet" />
+<nav class="sidebar">
+   <ul>
+      <li>Profile</li>
+      <li>Interventions</li>
+      <li>Materiels</li>
+      <li>Déconnections</li>
+   </ul>
+</nav>
