405bad161f978ac1f5dab0bd6a2882f6fc6df087
Ervi <dblervi@gmail.com>
2023-03-14 10:57:40 +0100

    C'est bon ?

--- profil/index.php
+++ profil/index.php
@@ -6,42 +6,61 @@
 
         <h1>Mon Profil</h1>
         <picture>
-
-            <img id="image-preview">
-            <h1>PHOTO DE PROFIL</h1>
-            <form id="form">
+            <form action="" method="post">
                 <label for="image">Choisir une image :</label>
                 <input type="file" id="pdp" name="pdp">
-                <br>
-                <img id="preview" src="#" alt="Aperçu de l'image" style="display:none; max-width: 400px;">
-                <br>
+                <p>Ajoutez votre Photo de Profil</p>
+
+
+                <div class="avatar" id="avatar" >
+            
+                    <img id="preview" src="#" alt="Aperçu de l'image">
+                </div>
+
                 <button type="button" id="delete" style="display:none;">Supprimer l'image</button>
             </form>
 
-
-
         </picture>
 
+        <form class="formulaire" action="" method="post">
 
-        <p>Ajoutez votre Photo de Profil</p>
+            <div>
+                <input type="text" id="nom" name="nom" class="peer" placeholder=" " required>
+                <label for="nom" class="peer-placeholder-shown:scale-100 peer-focus:-translate-y-6">Nom </label>
+            </div>
+            <div>
+                <input type="text" id="prenom" name="prenom" class="peer" placeholder=" " required>
+                <label for="prenom" class="peer-placeholder-shown:scale-100 peer-focus:-translate-y-6">Prénom </label>
+            </div>
+            <div>
+                <input type="email" id="email" name="email" class="peer" placeholder=" " required>
+                <label for="email" class="peer-placeholder-shown:scale-100 peer-focus:-translate-y-6">Adresse e-mail
+                </label>
+            </div>
+            <div>
+                <input type="text" id="Tel" name="Tel" class="peer" placeholder=" " required>
+                <label for="numero" class="peer-placeholder-shown:scale-100 peer-focus:-translate-y-6">Numéro de
+                    téléphone </label>
+            </div>
+            <div>
+                <input type="text" id="Postal" name="Postal" class="peer" placeholder=" " required>
+                <label for="Postal" class="peer-placeholder-shown:scale-100 peer-focus:-translate-y-6">Code Postal
+                </label>
+            </div>
+            <div>
+                <input type="text" id="Adresse" name="Adresse" class="peer" placeholder=" " required>
+                <label for="Adresse" class="peer-placeholder-shown:scale-100 peer-focus:-translate-y-6">Adresse </label>
+            </div>
+            <div>
+                <button type="submit" id="Envoyer" name="Envoyer">Envoyer</button>
+            </div>
+        </form>
 
+        <form class="formulaire" action="" method="post">
+            <div>
+                <button type="submit" id="Supprimer" name="Supprimer">Supprimer mon compte</button>
+            </div>
 
-        <form id="inscription-form">
-            <label for="nom">Nom :</label>
-            <br>
-            <input type="text" id="nom" name="nom" required>
-            <br>
-            <label for="prenom">Prénom :</label>
-            <br>
-            <input type="text" id="prenom" name="prenom" required>
-            <br>
-            <label for="email">Adresse e-mail :</label>
-            <br>
-            <input type="email" id="email" name="email" required>
-            <br>
-            <label for="numero">Numéro de téléphone :</label>
-            <br>
-            <input type="numero" id="numero" name="numero" required>
 
         </form>
 
